c0aa9e0410cb8afc2caaf05fb7236c7ae00f66cc
Mohammad Anwari <mdamt@mdamt.net>
2015-08-10 18:46:03 +0700

    Test using browserify

--- karma.conf.js
+++ karma.conf.js
@@ -1,67 +1,31 @@
-// Karma configuration
-// Generated on Mon Aug 10 2015 16:59:21 GMT+0700 (WIB)
+'use strict';
 
-module.exports = function(config) {
-  config.set({
+module.exports = function(karma) {
+  karma.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+    frameworks: [ 'jasmine', 'browserify' ],
 
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'requirejs'],
-
-
-    // list of files / patterns to load in the browser
     files: [
-      'test/test-main.js',
-      {pattern: 'src/**/*.js', included: false},
-      {pattern: 'test/**/*.js', included: false}
+      'test/**/*.js'
     ],
 
+    reporters: [ 'dots' ],
 
-    // list of files to exclude
-    exclude: [
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'test/**/*.js': [ 'browserify' ]
     },
 
+    browsers: [ 'Chrome' ],
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    logLevel: 'LOG_DEBUG',
 
+    singleRun: true,
+    autoWatch: false,
 
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
-  })
-}
+    // browserify configuration
+    browserify: {
+      debug: true,
+      transform: [ 'brfs', 'browserify-shim' ]
+    }
+  });
+};
