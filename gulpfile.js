ea998fda33f82a16ab85c4b356d5cc329d0a70e4
Herpiko Dwi Aguno <herpiko@gmail.com>
2015-09-18 22:28:09 +0700

    Bundling using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 'use strict';
 var gulp = require("gulp");
+var source = require("vinyl-source-stream");
+var browserify = require("browserify");
 var rimraf = require("gulp-rimraf");
 var htmlToJs = require("gulp-html-to-js");
+var minify = require("gulp-minify");
 
 gulp.task("clean", function(){
   return gulp.src("html/*.js", {read:false})
@@ -10,10 +13,17 @@ gulp.task("clean", function(){
 gulp.task("html", function(){
   return gulp.src("html/src/*")
     .pipe(htmlToJs({concat: 'html.js'}))
-    .pipe(gulp.dest("html"));
+    .pipe(minify())
+    .pipe(gulp.dest("./html/"));
+})
+gulp.task("browserify", function(){
+  return browserify("src/index.js")
+    .bundle()
+    .pipe(source("bundle.js"))
+    .pipe(gulp.dest("build"));
 })
 
-var tasks = ["clean", "html"];
+var tasks = ["clean", "html", "browserify"];
 gulp.task("default", tasks);
 
 
