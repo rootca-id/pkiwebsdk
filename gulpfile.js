494342fad6cc7d3d644e135af954c339b96efb3f
Herpiko Dwi Aguno <herpiko@gmail.com>
2015-09-26 12:14:16 +0700

    Inject css to UI's html

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,19 @@ var rimraf = require("gulp-rimraf");
 var htmlToJs = require("gulp-html-to-js");
 var minify = require("gulp-minify");
 var async = require("async");
+var inlineCss = require("gulp-inline-css");
 
 gulp.task("clean", function(){
   return gulp.src("html/*.js", {read:false})
     .pipe(rimraf({force:true}));
 })
+gulp.task("css", function(){
+  return gulp.src("html/src/*.html")
+    .pipe(inlineCss())
+    .pipe(gulp.dest("build"));
+})
 gulp.task("html", function(){
-  return gulp.src("html/src/*")
+  return gulp.src("build/*.html")
     .pipe(htmlToJs({concat: 'html.js'}))
     .pipe(minify())
     .pipe(gulp.dest("./html/"));
@@ -24,16 +30,12 @@ gulp.task("browserify", function(){
     .pipe(source("bundle.js"))
     .pipe(gulp.dest("build"));
 })
-gulp.task("css", function(){
-  return gulp.src("html/src/style.css")
-    .pipe(gulp.dest("build"));
-})
 gulp.task("cleanPostBuild", function(){
-  return gulp.src("html/html.js", {read:false})
+  return gulp.src(["html/html*", "html/html-min.js", "build/*.html"], {read:false})
     .pipe(rimraf({force:true}));
 })
 
-var tasks = ["clean", "html", "browserify", "css", "cleanPostBuild"];
+var tasks = ["clean", "css", "html", "browserify", "cleanPostBuild"];
 gulp.task("default", gulpSync.sync(tasks));
 
 
